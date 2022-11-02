5cda3abb91c3b627e6db7d32b2d20d0faa3afac9
Joiscilene <joisci.lene@hotmail.com>
2022-11-02 11:29:49 -0300

    Adicionando teste do relatório individual do produto

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,5 +1,28 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
+
+
+product = [
+    1,
+    "Cafe",
+    "Cafes Nature",
+    "2020-07-04",
+    "2023-02-09",
+    "FR48",
+    "instrucao"
+]
+
+
+phrases = (
+    "O produto Cafe"
+    " fabricado em 2020-07-04"
+    " por Cafes Nature com validade"
+    " até 2023-02-09"
+    " precisa ser armazenado instrucao."
+)
 
 
 def test_relatorio_produto():
     pass  # Seu teste deve ser escrito aqui
+
+    new_product = Product(*product)
+    assert str(new_product) == phrases
